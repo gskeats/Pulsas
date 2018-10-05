7d718fe11432d74441eb0bc43e0c31f30e347516
Graham <gnskeats@gmail.com>
2018-10-04 22:35:03 -0400

    First commit

--- Pulsas/Pulsas/Questions.swift
+++ Pulsas/Pulsas/Questions.swift
@@ -9,3 +9,18 @@
 import Foundation
 
 
+struct assessment {
+    let text:String
+    let pos_response:Int
+    let neg_response:Int
+    let not_sure:Int
+}
+
+
+let breathing_check:assessment=assessment(text: "Are they awake?", pos_response: 2, neg_response:0 , not_sure: 3)
+let consiousness_check:assessment=assessment(text: "Are they having trouble breathing or catching their breath?", pos_response: 0, neg_response:4 , not_sure: 5)
+let trauma_splitter:assessment=assessment(text:"Did they suffer an injury?", pos_response:6, neg_response:7, not_sure:-1)
+let alert_check:assessment=assessment(text: "Are they alert?", pos_response: 8, neg_response: 0, not_sure: 9)
+let serious_hemhorrage:assessment=assessment(text: "Is there serious bleeding?", pos_response: 0, neg_response: 1, not_sure: -1)
+let chestpain_check:assessment=assessment(text: "Are they complaing of any chest pain?", pos_response:11 , neg_response: 12, not_sure: -1)
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Pulsas
+0 is emergency exit scene
+1 is non emergency exit scene
+
+
+
+
+Artwork/Gifs from Fiver:
+Sternum Rub
+Emerg Exit Icon/Cartoon
+Non Emerg Exit Icon/Cartoon
