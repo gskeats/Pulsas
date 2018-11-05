133bce0a8410bac46e092a713e985ec46f6ac304
Graham <gnskeats@gmail.com>
2018-11-04 23:25:10 -0500

    Quick commit before classing branch
    Committing so that I can branch to check how a class based approach would work using a linked list.

--- Pulsas/Pulsas/ViewController.swift
+++ Pulsas/Pulsas/ViewController.swift
@@ -16,15 +16,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         QuestionText.text=question_array[current_question].text
         super.viewDidLoad()
-        
     }
     
-    @IBAction func NegResponse(_ sender: Any) {
-        QuestionText.text=question_array[0].text
+    
+    func getNext(flag:Bool)->Int{
+        let next_assesor:Int
+        if (flag){
+            next_assesor=question_array[current_question].pos_response
+            return next_assesor
+        }else{
+            next_assesor=question_array[current_question].neg_response
+            return next_assesor
+        }
+    
     }
     
-    @IBAction func PosRepsonse(_ sender: Any) {
-        QuestionText.text=question_array[1].text
+    @IBAction func negResponse(_ sender: Any) {
+        print(getNext(flag: false))
+        QuestionText.text=question_array[getNext(flag:false)].text
+    }
+    
+    @IBAction func posRepsonse(_ sender: Any) {
+        QuestionText.text=question_array[getNext(flag:true)].text
     }
 }
 
