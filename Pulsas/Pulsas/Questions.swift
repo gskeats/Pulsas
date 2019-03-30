48ce8ecfb0032e3492f8fde454bd34ffb6556344
Unknown <gnskeats@gmail.com>
2019-03-29 23:00:18 -0400

    Got things working
    Not pretty but got the transitions and objects working, can clean it up a little and need to add a start scene and can use a nil handler to get that back button working. TRY: create another inherited axiom display class for the start page?

--- Pulsas/Pulsas/Questions.swift
+++ Pulsas/Pulsas/Questions.swift
@@ -6,86 +6,112 @@
 //  Copyright © 2018 Graham Skeats. All rights reserved.
 //
 
-// lets just do three different objects with pointers between each, no inheritence
-
 import Foundation
 
 //base class since all views must display some information
-class Display{
+
+//lets do this classfully overriding, because there are only two possible segues from each scene if youre even performing a segue at all
+
+//information for yes or no assesment view
+//class for information display only views
+class Axiom_Display {
+    //var return_information:Assessment_Display
     var view_text:String
-    var return_segue:Display
-    init(text:String) {
+    var return_information:Axiom_Display?
+    init(text: String, last: Axiom_Display?) {
         view_text=text
+        return_information=last
+    }
+    func get_type(object:Axiom_Display)->Axiom_Display.Type{
+        let object_type=type(of: object)
+        return object_type
+    }
+    
+    func get_segue_identifier(object:Axiom_Display)->String{
+        switch object {
+        case is Assessment_Display:
+            return "toSimpleFromDisplay"
+        case is Assessment_with_Axiom:
+            return "toAxiomfromDisplay"
+        default:
+            return "noSegue"
+        }
+    }
+    
+    func get_neg()->Axiom_Display{
+        return self
+    }
+    
+    func get_pos()->Axiom_Display{
+        return self
     }
 }
 
-//information for yes or no assesment view
-class Assessment_Display {
-    var pos_response:Any
-    var neg_response:Any
-    var view_text:String
+class Assessment_Display:Axiom_Display{
+    var pos_response:Axiom_Display
+    var neg_response:Axiom_Display
     
-    init(text: String,pos_arg:Assessment_Display, neg_arg: Assessment_Display) {
+    init(text_init: String,pos_arg:Axiom_Display, neg_arg: Axiom_Display, last: Axiom_Display?) {
         pos_response=pos_arg
         neg_response=neg_arg
-        var view_text=text
+        super.init(text: text_init,last: last)
     }
-    func get_neg()->Any{
-        return self.neg_response
+    
+    override func get_neg()->Axiom_Display{
+        let next_question=self.neg_response
+        return next_question
     }
-    func get_pos()->Any{
-        return self.pos_response
+    
+    override func get_pos()->Axiom_Display{
+        let next_question=self.pos_response
+        return next_question
+
+    }
+    
+    override func get_segue_identifier(object: Axiom_Display) -> String {
+        switch object {
+        case is Assessment_with_Axiom:
+            return "toAxiomFromSimple"
+        case is Assessment_Display:
+            return "noSegue"
+        default:
+            return "toDisplayfromSimple"
+        }
     }
     
 }
 
 class Assessment_with_Axiom:Assessment_Display{
     let axiom:Axiom_Display
-    init(text: String, pos_arg: Assessment_Display, neg_arg: Assessment_Display,axiom_info:Axiom_Display) {
+    init(text: String, pos_arg: Axiom_Display, neg_arg: Axiom_Display,axiom_info:Axiom_Display,last:Axiom_Display?) {
         axiom=axiom_info
-        super.init(text: text, pos_arg: pos_arg, neg_arg: neg_arg)
+        super.init(text_init: text, pos_arg: pos_arg, neg_arg: neg_arg,last:last)
+    }
+    override func get_segue_identifier(object: Axiom_Display) -> String {
+            switch object {
+            case is Assessment_with_Axiom:
+                return "noSegue"
+            case is Assessment_Display:
+                return "toSimplefromAxiom"
+            default:
+                return "fromAxiomtoDisplay"
+            }
     }
 
 }
-//class for information display only views
-class Axiom_Display {
-    var return_information:Assessment_Display
-    var view_text:String
-    
-    init(text: String, exit:Assessment_Display) {
-        view_text=text
-        return_information=exit
 
-    }
-}
 
-let emerg_exit=Display(text: "Based on the information you have entered this is most likely a medical emergency.       Dial 911 IMMEDIATLEY.         Failure to do so in a timely manner may result in a loss of life.")
-let non_emerg_exit=Display(text: "Based on the information you have entered the application has not recognized this as a medical emergency.                This application is not indended to replace the advice of a physician, use common sense and seek further assistance if neccesary.              If the individual in question insists that they need emergency care listen to them, they are most likely right!              If symptoms do not abate then contact your physician for follow up care.")
+let emerg_exit=Axiom_Display(text: "Based on the information you have entered this is most likely a medical emergency.       Dial 911 IMMEDIATLEY.         Failure to do so in a timely manner may result in a loss of life.",last:nil)
+let non_emerg_exit=Axiom_Display(text: "Based on the information you have entered the application has not recognized this as a medical emergency.                This application is not indended to replace the advice of a physician, use common sense and seek further assistance if neccesary.              If the individual in question insists that they need emergency care listen to them, they are most likely right!              If symptoms do not abate then contact your physician for follow up care.", last: nil)
 
-let seizure_check=Assessment_Display(text: "Did they have a seizure?", pos_arg: , notsure_arg: false, neg_arg: non_emerg_exit)
 
-let csm_check=Assessment_Display(text: "Can they feel and move all of their fingers and toes?", pos_arg: seizure_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let tenderness_deformity_check=Assessment_Display(text: "Do they have any tenderness or deformity in their head, neck, or back?", pos_arg: csm_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let drugs_alochol_check=Assessment_Display(text: "Have they ingested any drugs or alcohol?", pos_arg: tenderness_deformity_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let head_neck_back_check=Assessment_Display(text: "Do they have any pain in their head neck or back?", pos_arg: drugs_alochol_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let trauma_alert_check=Assessment_Display(text: "Are they completely alert?", pos_arg: csm_check, neg_arg: non_emerg_exit, notsure_arg: true)
-let head_injury_check=Assessment_Display(text: "Did they inure their head?", pos_arg: trauma_alert_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let fall_greater_10ft_check=Assessment_Display(text: "Was the fall greater than 10ft?", pos_arg: head_injury_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let fall_check=Assessment_Display(text: "Did they fall?", pos_arg: fall_greater_10ft_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let behaving_abnormally_check=Assessment_Display(text: "Are they behaving abnormally?", pos_arg: fall_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let slurring_speech_check=Assessment_Display(text: "Are thye slurring their speech?", pos_arg: behaving_abnormally_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let hx_diabetes_check=Assessment_Display(text: "Do they have any history of diabetes?", pos_arg: slurring_speech_check, neg_arg: emerg_exit, notsure_arg: false)
-let faint_check=Assessment_Display(text: "Do they feel faint or like they might faint?", pos_arg: hx_diabetes_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let nausea_check=Assessment_Display(text: "Do they feel naseous?", pos_arg: emerg_exit, neg_arg: faint_check, notsure_arg: false)
-let clammy_coldsweats_check=Assessment_Display(text: "Are they clammy or experiencing cold sweats?", pos_arg: nausea_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let hx_cardiac_check=Assessment_Display(text: "Do they have any history of heart problems?", pos_arg: clammy_coldsweats_check, neg_arg: emerg_exit, notsure_arg: false)
-let female_age_check=Assessment_Display(text: "Are they a female and 45 years or older?", pos_arg: hx_cardiac_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let male_age_check=Assessment_Display(text: "Are they a male and 35 years old or older?", pos_arg: female_age_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let hx_stroke_check=Assessment_Display(text: "Do they have any history of stroke?", pos_arg: male_age_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let pain_above_navel_check=Assessment_Display(text: "Is the pain above or below the belly button?", pos_arg: hx_stroke_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let chest_pain_check=Assessment_Display(text: "Are they complaining of any chest pain?", pos_arg: pain_above_navel_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let first_alert_check=Assessment_Display(text: "Are they alert?", pos_arg: chest_pain_check, neg_arg: emerg_exit, notsure_arg: true)
-let serious_hemhorrage_check=Assessment_Display(text: "Is there serious bleeding?", pos_arg: chest_pain_check, neg_arg: emerg_exit, notsure_arg: false)
-let trauma_check=Assessment_Display(text: "Did they suffer an injury?", pos_arg: serious_hemhorrage_check, neg_arg: non_emerg_exit, notsure_arg: false)
-let breathing_check=Assessment_Display(text: "Are they having trouble catching their breath?", pos_arg: trauma_check, neg_arg: non_emerg_exit, notsure_arg: true)
-let consciousness_check=Assessment_Display(text: "Are they awake?", pos_arg: breathing_check, neg_arg: emerg_exit, notsure_arg: true)
+
+let trauma_check=Assessment_with_Axiom(text: "Are they injured?", pos_arg: emerg_exit, neg_arg: non_emerg_exit, axiom_info: sternum, last:nil)
+let sternum=Axiom_Display(text: "Rub their sternum motherfucker", last: trauma_check2)
+let trauma_check2=Assessment_Display(text_init: "Are they injured?", pos_arg: emerg_exit, neg_arg: non_emerg_exit,last:nil)
+
+
+let breathing_check=Assessment_Display(text_init: "Are they having difficulty breathng?", pos_arg: emerg_exit, neg_arg: trauma_check,last:nil)
+
+let consciousness_check=Assessment_Display(text_init: "Are they awake?", pos_arg: breathing_check , neg_arg: emerg_exit,last:nil)
+
--- Pulsas/Pulsas/ViewController.swift
+++ Pulsas/Pulsas/ViewController.swift
@@ -10,35 +10,63 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    @IBOutlet weak var SimpleQuestionText: UILabel!
+    @IBOutlet weak var scenetext: UILabel!
     
-    var current_question=consciousness_check
+    var passed_scene:Axiom_Display?
+    var current_scene=consciousness_check as Axiom_Display
 
     
     override func viewDidLoad() {
-        SimpleQuestionText.text=current_question.view_text
+        if passed_scene==nil {
+            scenetext.text=current_scene.view_text
+        } else{
+            scenetext.text=passed_scene?.view_text
+            current_scene=passed_scene!
+        }
+        
         super.viewDidLoad()
     }
     
-    
     @IBAction func negResponse(_ sender: Any) {
-        
+        let next_scene=current_scene.get_neg()
+        let segue_name=current_scene.get_segue_identifier(object: next_scene)
+        segue_programmatically(name: segue_name,next:next_scene)
     }
     
     @IBAction func posRepsonse(_ sender: Any) {
-
+        let next_scene=current_scene.get_pos()
+        let segue_name=current_scene.get_segue_identifier(object: next_scene)
+        segue_programmatically(name: segue_name,next:next_scene)
+        
     }
     
-    @IBAction func ReturnAxiom(_ sender: Any) {
+    @IBAction func backButton(_ sender: Any) {
+        let next_scene=current_scene.return_information
+        let segue_name=current_scene.get_segue_identifier(object: next_scene!)
+        segue_programmatically(name: segue_name, next: next_scene!)
+    }
+    
+    func segue_programmatically(name:String,next:Axiom_Display){
+        if name=="noSegue"{
+            current_scene=next
+            scenetext.text=current_scene.view_text
+            return
+        }
+        performSegue(withIdentifier: name, sender: next)
+    }
+
+
+    @IBAction func axiomButton(_ sender: Any) {
+        print("got here")
+        let downcast=current_scene as! Assessment_with_Axiom
+        let next_scene=downcast.axiom
+        let segue_name=current_scene.get_segue_identifier(object: next_scene)
+        segue_programmatically(name: segue_name, next: next_scene)
         
     }
-    @IBAction func AxiomDisplay(_ sender: Any) {
-        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination=segue.destination as? ViewController
+        destination?.passed_scene=(sender as? Axiom_Display)!
     }
 }
-
-
-
-
-
 
