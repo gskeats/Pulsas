e8db93b39e8e8c2b60d5802e47a928d5cdf96b6a
Graham <gnskeats@gmail.com>
2020-01-04 15:13:34 -0500

    Committing in a broken state to preserve QuestionClasses

--- Pulsas/Pulsas/QuestionClasses.swift
+++ Pulsas/Pulsas/QuestionClasses.swift
@@ -5,96 +5,99 @@
 //  Created by Graham Skeats on 9/22/19.
 //  Copyright © 2019 Graham Skeats. All rights reserved.
 //
-
+import UIKit
 import Foundation
-//base class since all views must display some information
 
-//lets do this classfully overriding, because there are only two possible segues from each scene if youre even performing a segue at all
-
-//information for yes or no assesment view
-//class for information display only views
-class Axiom_Display {
-    //var return_information:Assessment_Display
-    var view_text:String
-    var return_information:Axiom_Display?
-    init(text: String, last: Axiom_Display?) {
-        view_text=text
-        return_information=last
+class PulsasView{
+    let button_blue=UIColor(red: 194/255, green: 232/255, blue: 255/255, alpha:1.0)
+    let pulsas_blue=UIColor(red: 110/255, green: 214/255, blue: 255/255, alpha:1.0)
+    let screen_width=UIScreen.main.bounds.width
+    let screen_height=UIScreen.main.bounds.height
+    var label_text:String = ""
+    init(text_init: String){
+        label_text=text_init
     }
-    func get_type(object:Axiom_Display)->Axiom_Display.Type{
-        let object_type=type(of: object)
-        return object_type
+    func instantiate_view()->ViewController{
+        let view_controller=ViewController()
+        view_controller.view.backgroundColor=pulsas_blue
+        return view_controller
     }
-    
-    func get_segue_identifier(object:Axiom_Display)->String{
-        switch object {
-        case is Assessment_Display:
-            return "toSimpleFromDisplay"
-        case is Assessment_with_Axiom:
-            return "toAxiomfromDisplay"
-        default:
-            return "noSegue"
-        }
-    }
-    
-    func get_neg()->Axiom_Display{
+    func get_neg()->PulsasView{
         return self
     }
     
-    func get_pos()->Axiom_Display{
+    func get_pos()->PulsasView{
         return self
-    }
-}
-
-class Assessment_Display:Axiom_Display{
-    var pos_response:Axiom_Display
-    var neg_response:Axiom_Display
-    
-    init(text_init: String,pos_arg:Axiom_Display, neg_arg: Axiom_Display, last: Axiom_Display?) {
-        pos_response=pos_arg
-        neg_response=neg_arg
-        super.init(text: text_init,last: last)
-    }
-    
-    override func get_neg()->Axiom_Display{
-        let next_question=self.neg_response
-        return next_question
-    }
-    
-    override func get_pos()->Axiom_Display{
-        let next_question=self.pos_response
-        return next_question
-
-    }
-    
-    override func get_segue_identifier(object: Axiom_Display) -> String {
-        switch object {
-        case is Assessment_with_Axiom:
-            return "toAxiomFromSimple"
-        case is Assessment_Display:
-            return "noSegue"
-        default:
-            return "toDisplayfromSimple"
-        }
     }
     
 }
 
-class Assessment_with_Axiom:Assessment_Display{
-    let axiom:Axiom_Display
-    init(text: String, pos_arg: Axiom_Display, neg_arg: Axiom_Display,axiom_info:Axiom_Display,last:Axiom_Display?) {
-        axiom=axiom_info
-        super.init(text_init: text, pos_arg: pos_arg, neg_arg: neg_arg,last:last)
+class TextOnly:PulsasView{
+    
+    override init(text_init: String){
+        super.init(text_init: text_init)
     }
-    override func get_segue_identifier(object: Axiom_Display) -> String {
-            switch object {
-            case is Assessment_with_Axiom:
-                return "noSegue"
-            case is Assessment_Display:
-                return "toSimplefromAxiom"
-            default:
-                return "fromAxiomtoDisplay"
-            }
+    
+    override func instantiate_view()->ViewController{
+        let view_controller=ViewController()
+        let label = UILabel()
+        label.frame = CGRect(x: screen_width/3, y: screen_height/3, width: 200, height: 20)
+        label.text = "Are they awake?"
+        label.textColor = .black
+        view_controller.view.backgroundColor=pulsas_blue
+        view_controller.view.addSubview(label)
+        return view_controller
     }
 
+}
+
+class Basic_Question:TextOnly{
+    var pos_response:PulsasView
+    var neg_response:PulsasView
+    
+    init(text_init: String,pos_arg:PulsasView, neg_arg: PulsasView) {
+        self.pos_response=pos_arg
+        self.neg_response=neg_arg
+        super.init(text_init: text_init)
+    }
+    
+    override func instantiate_view()->ViewController{
+        let view_controller=ViewController()
+        let button_font = UIFont(name: "button_font", size: 40)
+
+        let yes_button=UIButton()
+        yes_button.setTitle("Yes", for: .normal)
+        yes_button.frame=CGRect(x: self.screen_width/9, y: self.screen_height*2/3, width: self.screen_width/3, height: self.screen_width/6)
+        yes_button.backgroundColor = self.button_blue
+        yes_button.titleLabel?.font=button_font
+        yes_button.setTitleColor(.black, for: .normal)
+        yes_button.layer.cornerRadius=18
+        yes_button.showsTouchWhenHighlighted=true
+
+
+
+        let no_button=UIButton()
+        no_button.frame=CGRect(x: self.screen_width*5/9, y: self.screen_height*2/3, width: self.screen_width/3, height: self.screen_width/6)
+        no_button.backgroundColor = self.button_blue
+        no_button.setTitle("No", for: .normal)
+        no_button.setTitleColor(.black, for: .normal)
+        no_button.layer.cornerRadius=18
+        no_button.showsTouchWhenHighlighted=true
+        no_button.addTarget(view_controller, action: #selector(view_controller.negResponse(_:)), for: .touchUpInside)
+        
+    
+        
+        view_controller.view.addSubview(yes_button)
+        view_controller.view.addSubview(no_button)
+        return view_controller
+    }
+    
+    override func get_neg()->PulsasView{
+        return self.neg_response
+    }
+    
+    override func get_pos()->PulsasView{
+        return self.pos_response
+    }
+    
 }
--- Pulsas/Pulsas/ViewController.swift
+++ Pulsas/Pulsas/ViewController.swift
@@ -12,66 +12,110 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var scenetext: UILabel!
     
-    var passed_scene:Axiom_Display?
-    var current_scene=consciousness_check as Axiom_Display
-
-    
     override func viewDidLoad() {
-        if passed_scene==nil {
-            scenetext.text=current_scene.view_text
-        } else{
-            scenetext.text=passed_scene?.view_text
-            current_scene=passed_scene!
-        }
-        
+        let basic_view=BasicView(text_init: "Hello")
+        basic_view.instantiate_view(view_controller: self)
         super.viewDidLoad()
     }
     
     @IBAction func negResponse(_ sender: Any) {
-        let next_scene=current_scene.get_neg()
-        let segue_name=current_scene.get_segue_identifier(object: next_scene)
-        segue_programmatically(name: segue_name,next:next_scene)
+        //current_scene=current_scene.get_neg() as! Basic_Question
     }
     
     @IBAction func posRepsonse(_ sender: Any) {
-        let next_scene=current_scene.get_pos()
-        let segue_name=current_scene.get_segue_identifier(object: next_scene)
-        segue_programmatically(name: segue_name,next:next_scene)
+        //current_scene=current_scene.get_pos() as! Basic_Question
         
-    }
-    
-    @IBAction func backButton(_ sender: Any) {
-        if current_scene.return_information==nil{
-            current_scene=consciousness_check as Axiom_Display
-            passed_scene=nil
-            performSegue(withIdentifier: "toSimpleFromDisplay", sender: current_scene)
-            return
-        }
-        let next_scene=current_scene.return_information
-        let segue_name=current_scene.get_segue_identifier(object: next_scene!)
-        segue_programmatically(name: segue_name, next: next_scene!)
-    }
-    
-    func segue_programmatically(name:String,next:Axiom_Display){
-        if name=="noSegue"{
-            current_scene=next
-            scenetext.text=current_scene.view_text
-            return
-        }
-        performSegue(withIdentifier: name, sender: next)
-    }
-
-
-    @IBAction func axiomButton(_ sender: Any) {
-        let downcast=current_scene as! Assessment_with_Axiom
-        let next_scene=downcast.axiom
-        let segue_name=current_scene.get_segue_identifier(object: next_scene)
-        segue_programmatically(name: segue_name, next: next_scene)
-        
-    }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destination=segue.destination as? ViewController
-        destination?.passed_scene=(sender as? Axiom_Display)!
     }
 }
 
+class PulsasView{
+    let button_blue=UIColor(red: 194/255, green: 232/255, blue: 255/255, alpha:1.0)
+    let pulsas_blue=UIColor(red: 110/255, green: 214/255, blue: 255/255, alpha:1.0)
+    let screen_width=UIScreen.main.bounds.width
+    let screen_height=UIScreen.main.bounds.height
+    var label_text:String = ""
+    init(text_init: String){
+        label_text=text_init
+    }
+    func instantiate_view(view_controller:ViewController){
+        view_controller.view.backgroundColor=pulsas_blue
+    }
+    func get_neg()->PulsasView{
+        return self
+    }
+    
+    func get_pos()->PulsasView{
+        return self
+    }
+    
+}
+
+class TextOnly:PulsasView{
+    
+    override init(text_init: String){
+        super.init(text_init: text_init)
+    }
+    
+    override func instantiate_view()->ViewController{
+        let view_controller=ViewController()
+        let label = UILabel()
+        label.frame = CGRect(x: screen_width/3, y: screen_height/3, width: 200, height: 20)
+        label.text = "Are they awake?"
+        label.textColor = .black
+        view_controller.view.backgroundColor=pulsas_blue
+        view_controller.view.addSubview(label)
+        return view_controller
+    }
+
+}
+
+class Basic_Question:TextOnly{
+    var pos_response:PulsasView
+    var neg_response:PulsasView
+    
+    init(text_init: String,pos_arg:PulsasView, neg_arg: PulsasView) {
+        self.pos_response=pos_arg
+        self.neg_response=neg_arg
+        super.init(text_init: text_init)
+    }
+    
+    override func instantiate_view()->ViewController{
+        let view_controller=ViewController()
+        let button_font = UIFont(name: "button_font", size: 40)
+
+        let yes_button=UIButton()
+        yes_button.setTitle("Yes", for: .normal)
+        yes_button.frame=CGRect(x: self.screen_width/9, y: self.screen_height*2/3, width: self.screen_width/3, height: self.screen_width/6)
+        yes_button.backgroundColor = self.button_blue
+        yes_button.titleLabel?.font=button_font
+        yes_button.setTitleColor(.black, for: .normal)
+        yes_button.layer.cornerRadius=18
+        yes_button.showsTouchWhenHighlighted=true
+
+
+
+        let no_button=UIButton()
+        no_button.frame=CGRect(x: self.screen_width*5/9, y: self.screen_height*2/3, width: self.screen_width/3, height: self.screen_width/6)
+        no_button.backgroundColor = self.button_blue
+        no_button.setTitle("No", for: .normal)
+        no_button.setTitleColor(.black, for: .normal)
+        no_button.layer.cornerRadius=18
+        no_button.showsTouchWhenHighlighted=true
+        no_button.addTarget(view_controller, action: #selector(view_controller.negResponse(_:)), for: .touchUpInside)
+        
+    
+        
+        view_controller.view.addSubview(yes_button)
+        view_controller.view.addSubview(no_button)
+        return view_controller
+    }
+    
+    override func get_neg()->PulsasView{
+        return self.neg_response
+    }
+    
+    override func get_pos()->PulsasView{
+        return self.pos_response
+    }
+    
+}
